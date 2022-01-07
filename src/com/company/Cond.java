17800389e0568ad5ed0896987646171e02477079
Jyoti <jyoti@infostride.com>
2022-01-07 12:49:45 +0530

    Day1

--- src/com/company/Cond.java
+++ src/com/company/Cond.java
@@ -1,9 +1,22 @@
 package com.company;
 
-public class Cond {
-    public static void main (String[]args)
-    {
-        if(5<6)
+//public class Cond {
+  //  public static void main (String[]args)
+    //{
+      //  if(5<6)
+        //{
+          //  System.out.println("1st Statement");
+        //}
+        //else
+        //{
+          //  System.out.println("2nd Statement");
+        //}
+    //}
+//
+
+public class Cond{
+    public static void main(String[]args){
+        if (5>6)
         {
             System.out.println("1st Statement");
         }
