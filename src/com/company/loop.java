66e0c41ef433a23961030bb0232929b8aac55809
Jyoti <jyoti@infostride.com>
2022-01-07 15:38:14 +0530

    Day1

--- src/com/company/loop.java
+++ src/com/company/loop.java
@@ -1,11 +1,20 @@
 package com.company;
 
-public class loop {
-    public static void main(String[]args){
+//public class loop {
+   // public static void main(String[]args){
+        //int i=1;
+      ///  do{
+          //  System.out.println(i);
+          //  i++;
+       // }while (i<=10);
+   // }
+
+public class loop{
+public static void main(String[]args){
         int i=1;
-        do{
-            System.out.println(i);
-            i++;
-        }while (i<=10);
-    }
-}
+        while(i<=10){
+        System.out.println(i);
+        i++;
+        }
+        }
+        }
