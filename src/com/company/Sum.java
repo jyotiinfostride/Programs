498aa2bc7fa3ac5249dad6c9368cf9be054e1813
Jyoti <jyoti@infostride.com>
2022-01-07 12:21:19 +0530

    Day1

---
+++ src/com/company/Sum.java
@@ -0,0 +1,21 @@
+package com.company;
+//class Sum {
+   // public static void main (String [] args)
+   // {
+       // int a=10;
+      //  int b=20;
+      //  System.out.println(a+b);
+   // }
+//
+
+class Sum{
+    public static void main (String [] args)
+    {
+        int a=20;
+        int b=30;
+        int c=40;
+        System.out.print(a+b+c);
+        int y;
+        y=10;
+    }
+}
