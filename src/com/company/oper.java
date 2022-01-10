ae8a7a0fd60cb3699260441e4766c7a60b995642
Jyoti <jyoti@infostride.com>
2022-01-10 21:22:31 +0530

    Day1

--- src/com/company/oper.java
+++ src/com/company/oper.java
@@ -1,15 +1,23 @@
 package com.company;
 
 public class oper {
-    public static void main(String[]args){
-        int a=12, b=12;
-        int result1, result2;
-        System.out.println("value of a:"+a);
-        result1= ++a;
-        System.out.println("After increment:"+result1);
-        System.out.println("value of b:"+b);
-        result2= --b;
-        System.out.println("After decrement:"+result2);
+   // public static void main(String[]args){
+     //   int a=12, b=12;
+       // int result1, result2;
+        //System.out.println("value of a:"+a);
+        //result1= ++a;
+        //System.out.println("After increment:"+result1);
+        //System.out.println("value of b:"+b);
+        //result2= --b;
+        //System.out.println("After decrement:"+result2);
 
-    }
+    //}
+//}
+
+public static void main(String[]args){
+    int februaryDays= 29;
+    String result;
+    result=(februaryDays ==28)?"Not a leap year":"Leap year";
+    System.out.println(result);
+}
 }
