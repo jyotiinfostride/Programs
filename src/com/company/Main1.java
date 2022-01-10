842fb21cf5bda4bcb5b6785e8a1c6d53bc328af8
Jyoti <jyoti@infostride.com>
2022-01-10 20:30:49 +0530

    Day1

--- src/com/company/Main1.java
+++ src/com/company/Main1.java
@@ -1,22 +1,30 @@
 package com.company;
 
 public class Main1 {
-    public static void main(String[]args){
-        int a=12;
-        int b=5;
-        int c =a+b;
-        int d =a-b;
-        int e =a*b;
-        int f =a/b;
-        int g =a%b;
+   // public static void main(String[]args){
+     //   int a=12;
+       // int b=5;
+        //int c =a+b;
+        //int d =a-b;
+        //int e =a*b;
+        //int f =a/b;
+        //int g =a%b;
 
 
-        System.out.println("c="+c );
-        System.out.println("d="+d );
-        System.out.println("e="+e);
-        System.out.println("f="+f);
-        System.out.println("g="+g);
+        //System.out.println("c="+c );
+        //System.out.println("d="+d );
+        //System.out.println("e="+e);
+        //System.out.println("f="+f);
+        //System.out.println("g="+g);
 
 
-    }
+    //}
+//}
+
+public static void main(String[]args){
+          int a=4;
+          int var;
+          var =a;
+          System.out.println("var using =: "+var);
+}
 }
