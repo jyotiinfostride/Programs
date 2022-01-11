11d570ffc3708b8edf1020b41a7d3cebca84479b
Jyoti <jyoti@infostride.com>
2022-01-11 16:31:52 +0530

    Day1

---
+++ src/com/company/assignment.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class assignment {
+    public static void main(String[]args){
+        int num;
+        num=10;
+        System.out.println("num is assigned;"+ num);
+    }
+}
