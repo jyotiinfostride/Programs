2832b9c820e92ad1a3ea0567ee1390858d62fdef
Jyoti <jyoti@infostride.com>
2022-01-11 16:50:50 +0530

    Day1

--- src/com/company/assignment.java
+++ src/com/company/assignment.java
@@ -8,13 +8,22 @@ public class assignment {
     //}
 //}
 
-public static void main(String[]args){
-    int num;
-    String name;
-    num=20;
-    name= "Hello";
-    System.out.println("num is assigned;"+ num);
-    System.out.println("name is assigned;"+ name);
+//public static void main(String[]args){
+    //int num;
+    //String name;
+    //num=20;
+    //name= "Hello";
+    //System.out.println("num is assigned;"+ num);
+    //System.out.println("name is assigned;"+ name);
 
+//}
+//}
+
+public static void main(String[]args){
+    int num1,num2;
+     num1=20;
+    num2= 30;
+    System.out.println("num is assigned:"+num1);
+    System.out.println("num is assigned:"+num2);
 }
 }
