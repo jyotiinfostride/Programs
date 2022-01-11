ee7b9ad74eb4c40d3d0bf2846e54aca4a1563720
Jyoti <jyoti@infostride.com>
2022-01-11 20:44:26 +0530

    Day1

---
+++ src/com/company/length.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class length {
+    //public static void main(String[]args){
+    //String s1="Javatpoint";
+    //String s2="Python";
+    //  System.out.println("String length is:"+s1.length());
+    //    System.out.println("String length is:"+s2.length());
+    //  }
+//}
+    public static void main(String[] args) {
+        String s1 = "Greek for Greek";
+        String s2 = "Keywords";
+        System.out.println("String length is:" + s1.length());
+        System.out.println("String length is:" + s2.length());
+    }
+}
