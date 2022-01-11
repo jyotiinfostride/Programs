8716c466dea26dca7acb9ba93182fe533a1ac891
Jyoti <jyoti@infostride.com>
2022-01-11 16:45:42 +0530

    Day1

--- src/com/company/assignment.java
+++ src/com/company/assignment.java
@@ -1,9 +1,20 @@
 package com.company;
 
 public class assignment {
-    public static void main(String[]args){
-        int num;
-        num=10;
-        System.out.println("num is assigned;"+ num);
-    }
+    //public static void main(String[]args){
+      //  int num;
+        //num=10;
+        //System.out.println("num is assigned;"+ num);
+    //}
+//}
+
+public static void main(String[]args){
+    int num;
+    String name;
+    num=20;
+    name= "Hello";
+    System.out.println("num is assigned;"+ num);
+    System.out.println("name is assigned;"+ name);
+
+}
 }
