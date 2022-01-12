abc6ef430022171d0de97f6c30ea27ebb1849236
Jyoti <jyoti@infostride.com>
2022-01-12 15:32:15 +0530

    Day1

---
+++ src/com/company/MyClass.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MyClass {
+
+   int num;
+     MyClass() {
+         num = 100;
+     }
+     public static void main(String[]args)
+     {
+         MyClass obj= new MyClass();
+         System.out.println(obj.num);
+     }
+}
+
