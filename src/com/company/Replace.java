bd6c8b8ac3dd2838d26ba1d59b401a6cacec0695
Jyoti <jyoti@infostride.com>
2022-01-12 12:19:02 +0530

    Day1

--- src/com/company/Replace.java
+++ src/com/company/Replace.java
@@ -1,9 +1,16 @@
 package com.company;
 
 public class Replace {
-    public static void main(String[]args){
-        String s1="The quick brown for jumps over the lazy dog";
-                String replaceString = s1.replace('d','f');
-        System.out.println(replaceString);
-    }
+   // public static void main(String[]args){
+     //   String s1="The quick brown for jumps over the lazy dog";
+       //         String replaceString = s1.replace('d','f');
+        //System.out.println(replaceString);
+    //}
+//}
+
+public static void main(String[]args){
+    String s1="Harry";
+    String replaceString =s1.replace('r','e');
+    System.out.println(replaceString);
+}
 }
