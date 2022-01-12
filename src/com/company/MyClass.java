67f81045c2a80872b161092c88ef93e449be55c9
Jyoti <jyoti@infostride.com>
2022-01-12 16:21:51 +0530

    Day1

--- src/com/company/MyClass.java
+++ src/com/company/MyClass.java
@@ -2,14 +2,27 @@ package com.company;
 
 public class MyClass {
 
-   int num;
-     MyClass() {
-         num = 100;
-     }
-     public static void main(String[]args)
-     {
-         MyClass obj= new MyClass();
-         System.out.println(obj.num);
-     }
+   //int num;
+     //MyClass() {
+       //  num = 100;
+     //}
+     //public static void main(String[]args)
+     //{
+      //   MyClass obj= new MyClass();
+    //     System.out.println(obj.num);
+  //   }
+//}
+
+ int a;
+   String name;
+MyClass()
+{
+    a=10;
+    name="null";
+}
+public static void main(String[]args){
+    MyClass obj = new MyClass();
+    System.out.println(obj.a+" "+obj.name);
+}
 }
 
